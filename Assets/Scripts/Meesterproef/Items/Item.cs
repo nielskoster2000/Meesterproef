3b5b35233a3d3a8b9c39a849548e44c1ac3e8e45
Niels Koster <niels-koster@outlook.com>
2022-02-28 16:30:09 +0100

    Small fix to missing item script

---
+++ Assets/Scripts/Meesterproef/Items/Item.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string _name;
+
+    public virtual void Use()
+    {
+
+    }
+}
