682de3ae438a6b3881939291230ecb784855da54
Niels Koster <niels-koster@outlook.com>
2022-03-03 17:41:58 +0100

    Improved bot spawning
    Will add spawn points later to simplify the process of positioning

--- Assets/Scripts/Meesterproef/Managers/GameManager.cs
+++ Assets/Scripts/Meesterproef/Managers/GameManager.cs
@@ -8,24 +8,29 @@ public class GameManager : MonoBehaviour
     [SerializeField] List<Humanoid> players = new List<Humanoid>();
     GameObject playersParent;
 
+    int botCount = 0;
+
     private void Start()
     {
         playersParent = GameObject.Find("Players");
-        SpawnBot("Test", new Vector3(-1.5f, -0.15f, -3f), Vector3.zero, new List<Ability>());
+        SpawnBot("Steve", new Vector3(-1.5f, -0.15f, -3f), Vector3.zero);
+        SpawnBot("Micheal", new Vector3(10f, -0.15f, -1f), Vector3.zero);
     }
 
     private void SpawnBots(int botcount)
     {
-
     }
 
-    private void SpawnBot(string botname = "bot", Vector3 pos = default(Vector3), Vector3 rotation = default(Vector3), List<Ability> abilities = null)
+    private void SpawnBot(string botname = "bot", Vector3 pos = default(Vector3), Vector3 rotation = default(Vector3))
     {
         GameObject bot = Instantiate<GameObject>(Resources.Load<GameObject>("Bot"));
 
         //Add bot to players 
         bot.transform.parent = playersParent.transform;
         players.Add(bot.GetComponent<Humanoid>());
+
+        //Set bot cam target display
+        bot.GetComponentInChildren<Camera>().targetDisplay = botCount + 1;
 
         //Set name
         bot.GetComponent<Humanoid>().username = botname;
@@ -34,17 +39,6 @@ public class GameManager : MonoBehaviour
         if (pos != null) { bot.transform.position = pos; } else { bot.transform.position = default(Vector3); }
         if (rotation != null) { bot.transform.rotation = Quaternion.Euler(rotation); } else { bot.transform.rotation = Quaternion.Euler(default(Vector3)); }
 
-        //Add abilities 
-        if (abilities != null)
-        {
-            foreach (Ability ability in abilities)
-            {
-                Ability ab = bot.AddComponent<Ability>(); 
-                ab = ability;
-            }
-        }
-/*        if (ability1 != null) {  Ability ab = bot.AddComponent<Ability>(); ab = ability1; }
-        if (ability2 != null) {  Ability ab = bot.AddComponent<Ability>(); ab = ability2; }
-        if (ability3 != null) {  Ability ab = bot.AddComponent<Ability>(); ab = ability3; }*/
+        botCount++;
     }
 }
