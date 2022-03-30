8f06127b0b2cd9e5c9bd2d3da8a143e195c17e9f
Niels Koster <niels-koster@outlook.com>
2022-03-30 12:30:59 +0200

    Misc changes

---
+++ Assets/Scripts/Other/TODO.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TODO : MonoBehaviour
+{
+    [SerializeField] List<string> tasks = new List<string>();
+}
