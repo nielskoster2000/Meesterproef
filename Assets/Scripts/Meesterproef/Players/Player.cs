4f932f9db96e01136d2ec65ea2b311a23e22110d
Niels Koster <niels-koster@outlook.com>
2022-04-23 10:28:36 +0200

    Player now handles stats instead of humanoid

--- Assets/Scripts/Meesterproef/Players/Player.cs
+++ Assets/Scripts/Meesterproef/Players/Player.cs
@@ -2,21 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player
 {
     [HideInInspector] public string username;
     [HideInInspector] public Humanoid humanoid;
     Vector2 stats;
+
+    [HideInInspector] public LeaderboardSlot leaderboardSlot;
 
     public bool IsHuman { get; private set; } = false;
 
     public Player(string name = "bot")
     {
         username = name;
+        //humanoid.OnDeath.AddListener(AddDeath);
     }
 
     void SetUserName()
     {
         username = Settings.username;
+    }
+
+    public void AddKill()
+    {
+        stats.x++;
+
+        if (stats.x >= Settings.MatchMaxKills)
+        {
+            humanoid.OnMaxKillsReached.Invoke();
+        }
+    }
+
+    public void AddDeath(Player player)
+    {
+        stats.y++;
+    }
+
+    public Vector2 GetStats()
+    {
+        return stats;
     }
 }
