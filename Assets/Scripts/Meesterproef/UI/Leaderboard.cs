df6a07bf4a11b95ae56beb91beb45323511654d3
Niels Koster <niels-koster@outlook.com>
2022-04-23 10:31:36 +0200

    Leaderboard script is now functional

--- Assets/Scripts/Meesterproef/UI/Leaderboard.cs
+++ Assets/Scripts/Meesterproef/UI/Leaderboard.cs
@@ -16,18 +16,99 @@ public class Leaderboard : MonoBehaviour
     GameObject lbFFA;
     GameObject lbTDM;
 
+    GameObject FFASlots;
+    //GameObject TDMSlots;
 
-    private void Awake()
+    [SerializeField] GameObject leaderboardSlot;
+    public List<LeaderboardSlot> leaderboardSlots = new List<LeaderboardSlot>();
+
+    private void SetObjects()
     {
         lbFFA = transform.GetChild(0).gameObject;
         lbTDM = transform.GetChild(1).gameObject;
 
-        ShowGamemodeLeaderboard();
+        FFASlots = lbFFA.transform.Find("Slots").gameObject;
+    }
+
+    public void SetLeaderboardSlots()
+    {
+        foreach (Player player in GameManager.players)
+        {
+            AddSlot(player);
+        }
+    }
+
+    public void Clear()
+    {
+        foreach (LeaderboardSlot lbslot in leaderboardSlots)
+        {
+            Destroy(lbslot);
+        }
+
+        leaderboardSlots.Clear();
     }
 
     public void ShowGamemodeLeaderboard()
     {
         lbFFA.SetActive(System.Convert.ToBoolean(currentGameMode));
         lbTDM.SetActive(!System.Convert.ToBoolean(currentGameMode));
+    }
+
+    public void AddSlot(Player player)
+    {
+        if (FFASlots == null)
+        {
+            SetObjects();
+        }
+
+        GameObject newLeaderboardSlot = Instantiate(leaderboardSlot, FFASlots.transform);
+        LeaderboardSlot leaderboardSlotComponent = newLeaderboardSlot.GetComponent<LeaderboardSlot>();
+        leaderboardSlotComponent.SetPlayerStats(player);
+        leaderboardSlots.Add(leaderboardSlotComponent);
+
+        //Sort it correctly
+    }
+
+    public void RemoveSlot(int index)
+    {
+        Destroy(leaderboardSlots[index]);
+        leaderboardSlots.RemoveAt(index);
+    }
+
+    public void SortSlot(Player player)
+    {
+        for (int i = leaderboardSlots.Count - 1; i > 0; i--)
+        {
+            if (leaderboardSlots[i].player.GetStats().x < player.GetStats().x)
+            {
+                player.leaderboardSlot.transform.SetSiblingIndex(player.leaderboardSlot.transform.GetSiblingIndex() - 1);
+
+                leaderboardSlots.RemoveAt(i);
+                leaderboardSlots.Insert(i-1, player.leaderboardSlot);
+
+                SortSlot(player);
+            }
+        }
+    }
+
+    public void SortSlots(Player player = null, Player killer = null)
+    {
+        int mostkills = -1;
+
+        foreach (LeaderboardSlot lbslot in leaderboardSlots)
+        {
+            lbslot.SetPlayerStats(lbslot.player);
+
+            if (lbslot.player.GetStats().x > mostkills)
+            {
+                lbslot.gameObject.transform.SetAsFirstSibling();
+                mostkills = Mathf.RoundToInt(lbslot.player.GetStats().x); 
+            }
+        }
+    }
+
+    public void UpdatePlayerSlot(Player player, int index)
+    {
+        leaderboardSlots[index].SetPlayerStats(player);
     }
 }
--- Assets/Scripts/Meesterproef/UI/LeaderboardSlot.cs
+++ Assets/Scripts/Meesterproef/UI/LeaderboardSlot.cs
@@ -2,39 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class LeaderboardSlot : MonoBehaviour
 {
-    Humanoid playerHumanoid;
+    public Player player;
 
     Text lbPlayername;
     Text lbkills;
     Text lbdeaths;
 
-    private void Awake()
+    Leaderboard leaderboard;
+
+    public void SetComponents()
     {
         transform.GetChild(0).TryGetComponent<Text>(out lbPlayername);
         transform.GetChild(1).TryGetComponent<Text>(out lbkills) ;
         transform.GetChild(2).TryGetComponent<Text>(out lbdeaths);
+
+        leaderboard = GameManager.FindComponentInParentRecursive(transform, typeof(Leaderboard)) as Leaderboard;
     }
 
-    public void GetPlayerStats(Player player)
+    public void SetPlayerStats(Player p)
     {
-        if (TryGetComponent<Humanoid>(out Humanoid humanoid))
+        player = p;
+
+        if (lbdeaths == null)
         {
-            lbPlayername.text = player.username;
-            playerHumanoid = player.humanoid;
-
-            if (TryGetComponent<User>(out _))
-            {
-                lbPlayername.text = Settings.username;
-            }
+            SetComponents();
         }
-    }
 
-    public void UpdatePlayerStats()
-    {
-        lbkills.text = playerHumanoid.GetStats().x.ToString();
-        lbdeaths.text = playerHumanoid.GetStats().y.ToString();
+        lbPlayername.text = player.username;
+        lbkills.text = player.GetStats().x.ToString();
+        lbdeaths.text = player.GetStats().y.ToString();
     }
 }
